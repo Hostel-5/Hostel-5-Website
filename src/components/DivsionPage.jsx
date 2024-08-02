c9955110533b45a6bb81858b3212e597d1f51fd1
Shubham Raj <shubham282raj@gmail.com>
2024-08-02 13:58:25 +0530

    Responsive division page

--- src/components/DivsionPage.jsx
+++ src/components/DivsionPage.jsx
@@ -34,32 +34,42 @@ export default function DivsionPage() {
   }
 
   return (
-    <div className="max-w-screen-xl m-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 justify-items-center flex-wrap gap-4 mt-4">
+    <div className="max-w-screen-lg m-auto grid grid-cols-1 justify-items-center flex-wrap gap-4 mt-4 ">
       {sheet.map((data) => (
         <Link
           to={`/${division}/${data.postID}`}
-          className="flex flex-col bg-slate-700 max-w-sm"
+          className="flex flex-col sm:grid sm:grid-cols-3 sm:aspect-[3] sm:overflow-hidden bg-slate-700 max-w-full relative"
           key={division + "-image-" + data.postID}
         >
-          <div className="aspect-w-4 aspect-h-3 aspect-square">
+          <div className="w-full aspect-w-4 aspect-h-3 aspect-square">
             <img
               src={data.image}
               alt={`${division}-image-${data.postID}`}
               className="object-cover"
             />
           </div>
-          <div className="line-clamp-5 p-3 pb-0">
-            <div className="text-2xl font-bold text-white">{data.heading}</div>
-            <ReactMarkdown
-              children={data.description}
-              className={
-                "prose prose-invert !max-h-none break-words overflow-y-clip"
-              }
-              remarkPlugins={[remarkGfm]}
-            />
-          </div>
-          <div className="m-auto border my-2 w-32 text-center text-white rounded-lg">
-            Read More
+          <div className="col-span-2">
+            <div className="line-clamp- p-3 pb-0">
+              <div className="text-2xl font-bold text-white">
+                {data.heading}
+              </div>
+              <ReactMarkdown
+                children={data.description}
+                className={
+                  "prose prose-invert line-clamp-4 sm:line-clamp-none !max-w-none break-words overflow-y-clip"
+                }
+                remarkPlugins={[remarkGfm]}
+              />
+            </div>
+            <div className="sm:absolute sm:bottom-0 sm:left-2/3 sm:-translate-x-1/2 sm:z-20 m-auto border my-2 w-32 text-center text-white rounded-lg bg-slate-800">
+              Read More
+            </div>
+            <div
+              style={{
+                mask: "linear-gradient(transparent, black)",
+              }}
+              className="hidden sm:block w-2/3 absolute bottom-0 right-0 h-20 bg-slate-900 backdrop-blur-sm z-10"
+            ></div>
           </div>
         </Link>
       ))}
